60e6e3f9f335bcba8af6bdef4461a1676eedecae
Adonay Demewez Gebremedhin <adonaydemewez1@gmail.com>
2025-10-04 10:15:45 +0400

    pub3

--- daily-flow-struct/src/integrations/supabase/client.ts
+++ daily-flow-struct/src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://zhomqccxumwmabuzmpuu.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpob21xY2N4dW13bWFidXptcHV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MjkwMjEsImV4cCI6MjA3NTAwNTAyMX0.wSkgKqSVNSIXTC0PEub2sfL94n9OyKFUqzHVRJoCu1U";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
